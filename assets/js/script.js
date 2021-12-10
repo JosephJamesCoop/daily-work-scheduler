fe8c3e76649522b3afaa75b251aae7168f52573a
JosephJamesCoop <91605342+JosephJamesCoop@users.noreply.github.com>
2021-12-10 15:17:50 -0800

    time coded colors for tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,48 @@ $(document).ready(function () {
 
 });
 
+var currentTime = moment().format("HH");
+if (currentTime > 12) {
+  currentTime = currentTime - 12;
+}
+else {
+  currentTime = currentTime
+}
 
+$(".timePm").each(function () {
+  var classTime = $(this).attr("id").split("-")[1];
+  console.log(classTime);
+  console.log(currentTime);
+  if (currentTime == classTime) {
+    $(this).addClass("present");
+  } else if (currentTime < classTime) {
+    $(this).removeClass("present");
+    $(this).addClass("future");
+    console.log(this);
+  } else if (currentTime > classTime) {
+    $(this).removeClass("future");
+    $(this).addClass("past");
+  }
+});
+
+$(".timeAm").each(function () {
+  var classTime = $(this).attr("id").split("-")[1];
+  console.log(classTime);
+  console.log(currentTime);
+  if (currentTime == classTime) {
+    $(this).addClass("present");
+  } else if (currentTime > 6 && currentTime < classTime) {
+    $(this).removeClass("present");
+    $(this).addClass("future");
+    console.log(this);
+  } else if (currentTime > 6 && currentTime > classTime) {
+    $(this).removeClass("future");
+    $(this).addClass("past");
+  } else if (currentTime < 6) {
+    $(this).removeClass("future");
+    $(this).addClass("past");
+  }
+});
 // select all each slot next to time and make the <textarea> data save
 
 // select each time slot run through for loop to change color based ont ime
