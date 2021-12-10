178271ae3e21ea9cbeae32b4f0d6d99ad8c68773
JosephJamesCoop <91605342+JosephJamesCoop@users.noreply.github.com>
2021-12-10 14:42:58 -0800

    added current date to header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,21 @@
-// I will need this in order to edit the taska nd call upoin the p function
-// $(".list-group").on("click", "p", function() {
-//   console.log("<p> was clicked");
-// });
 
-
-// additional info
-// this will console log whatever the text was in the p element that was clicked.
-
-// $(".list-group").on("click", "p", function() {
-//     var text = $(this).text();
-//     console.log(text);
-//   });
-
-//this is compelte solution that replaces with written tect.
-
-// $(".list-group").on("click", "p", function() {
-//     var text = $(this)
-//     .text()
-//     .trim();
-   
-//     var textInput = $("<textarea>")
-//       .addClass("form-control")
-//       .val(text);
-  
-//     $(this).replaceWith(textInput);
-//   });
-
-
-// this is used to cancel the edit.
-// $(".list-group").on("blur", "textarea", function() {
-//     // get the textarea's current value/text
-//   var text = $(this)
-//   .val()
-//   .trim();
-  
-//   // get the parent ul's id attribute
-//   var status = $(this)
-//   .closest(".list-group")
-//   .attr("id")
-//   .replace("list-", "");
-  
-//   // get the task's position in the list of other li elements
-//   var index = $(this)
-//   .closest(".list-group-item")
-//   .index();
-  
-//   tasks[status][index].text = text;
-//   saveTasks();
-//   });
-
-
-
-
-  // if function to filter date
-
-// if (time < current-time){
-// class = red }
-//     if else (time > current-time){
-// class = green }
-//     else {
-//         class = gray
-//     }
-
-
-// notes 
 
 // select p element with class of date and add current date to be dispalyed.
 
-// select all time slots and make them editable in <textarea> part
+$(document).ready(function () {
+  //Moment.js code for current date and time
+  var currentDay = document.getElementById("currentDay");
+  var currentDate = moment().format("LLLL");
+  currentDay.innerHTML = currentDate;
+
+});
+
+
+// select all each slot next to time and make the <textarea> data save
 
 // select each time slot run through for loop to change color based ont ime
 
-//
+
+
+
+
